b0b57dbc9aaf46e022646970bac237a55cd254a7
Art_Mainardi <iensenmainardi@gmail.com>
2026-06-22 12:04:03 -0300

    Método PUT

--- app.js
+++ app.js
@@ -1,57 +1,58 @@
 // Importa o framework express (Analogia: como se importasse o SpringBoot):
 const express = require('express');
 
-
 // Cria a aplicação do servidor:
 const server = express(); // Variável que irá receber as requisições
+let curso = [];
 
 
-// Define uma rota do método GET:
-// '/curso'-> endpoint    |    req-> recebe    |    res-> responde
-server.get('/curso', (req, res) => {
-    // Criando o objeto:
-    let objeto = {
-        propriedade: 'valor'
-    };
-
-    // Retornando o objeto:
-    return res.json(objeto);
-
-    /* Forma encurtada: return res.json({propriedade: 'valor'}); */
-}); 
-
-
-// Método listen() faz o servidor escutar as requisições em uma porta determinada:
-server.listen(8050, () => {
-    console.log("O servidor está ligado na porta 8050");
-});
-
-
-// Query params
+/*// Query params
 server.get('/curso', (req, res) => {
     const nome = req.query.nome;
     return res.json({
         curso: `Aprendendo ${nome}`
     })
 });
-
-
 // Route params
 server.get('/curso/:id', (req, res) => {
     const id = req.params.id;
     return res.json({
         curso: `Aprendendo ${id}`
     })
-});
+});*/
 
+
+// ========= MÉTODOS ========= //
+// Método GET:
+// '/curso'-> endpoint    |    req-> recebe    |    res-> responde
+server.get('/curso', (req, res) => {
+    // Retornando o objeto:
+    return res.json(curso);
+
+    /* Forma encurtada: return res.json({propriedade: 'valor'}); */
+}); 
 
 // Método Post:
 server.use(express.json()); // Permite que o Express entenda JSON
 server.use(express.urlencoded({ extended: true })); // Permite que entenda dados de formulários (URL-encoded)
 
-let curso = [];
-server.post(`/add-curso`, (req, res) => {
+server.post(`/curso`, (req, res) => {
     const objeto = req.body;
     curso.push(objeto.nome);
     return res.json(curso);
+});
+
+// Método Put:
+server.put(`/curso/:id`, (req, res) => {
+    const id = req.params.id;
+    const objeto = req.body;
+
+    curso[id] = objeto.nome;
+    return res.json(curso);
+});
+
+
+// Método listen() faz o servidor escutar as requisições em uma porta determinada:
+server.listen(8050, () => {
+    console.log("O servidor está ligado na porta 8050");
 });
